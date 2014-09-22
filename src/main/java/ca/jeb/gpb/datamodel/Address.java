413ddada117f2d064ae29a7ba34bd4a04d9cd765
ebourgeois <boureric@ms.com>
2014-09-22 18:36:26 -0400

    New serialization example

---
+++ src/main/java/ca/jeb/gpb/datamodel/Address.java
@@ -0,0 +1,13 @@
+// Copyright (c) 2014 Erick Bourgeois, All Rights Reserved
+
+package ca.jeb.gpb.datamodel;
+
+/**
+ * POJO representing an Address
+ * 
+ * @author <a href="mailto:erick@jeb.ca">Erick Bourgeois</a>
+ */
+public class Address
+{
+
+}
