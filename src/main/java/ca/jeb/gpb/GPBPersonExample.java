576d359b71d9dad9b2bb293ae2c416648f78f13d
Erick Bourgeois <erick@jeb.ca>
2014-09-22 11:35:03 -0400

    initial commit

---
+++ src/main/java/ca/jeb/gpb/GPBPersonExample.java
@@ -0,0 +1,20 @@
+// Copyright (c) 2014 Erick Bourgeois, All Rights Reserved
+
+package ca.jeb.gpb;
+
+import ca.jeb.generated.proto.Message.Person;
+
+/**
+ * @author <a href="mailto:erick@jeb.ca">Erick Bourgeois</a>
+ */
+public class GPBPersonExample
+{
+  /**
+   * @param args
+   */
+  public static void main(String[] args)
+  {
+    final Person.Builder personBuilder = Person.newBuilder();
+
+  }
+}
